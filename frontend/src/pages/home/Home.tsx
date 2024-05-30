82d55a2b18c5a0f1e352d5ba4afe6c39f2d3771b
wyntia <gosia.chyrza@gmail.com>
2024-05-30 15:46:15 +0200

    fetching data frontend

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
-import {MainNavbar} from "../../components/MainNavbar.tsx";
-import {title} from "../../util/title.ts";
+import { MainNavbar } from "../../components/MainNavbar.tsx";
+import { title } from "../../util/title.ts";
 import useApi from "../../hooks/useApi.ts";
-import {Row} from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import style from "./Home.module.scss";
 import {
@@ -12,88 +12,130 @@ import {
   Legend,
   ResponsiveContainer,
   AreaChart,
-  Area, PieChart, Pie, Radar, PolarAngleAxis, PolarRadiusAxis, PolarGrid, RadarChart
+  Area,
+  PieChart,
+  Pie,
+  Radar,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  PolarGrid,
+  RadarChart,
 } from "recharts";
 import VehiclesBarChart from "./components/VehiclesBarChart.tsx";
 import MapAndTable from "./components/MapAndTable.tsx";
 
 // todo its just an sample data for charts, remove it
-const SAMPLE_DATA = Array.from({length: 16}, (_, i) => ({
+const SAMPLE_DATA = Array.from({ length: 16 }, (_, i) => ({
   name: `name-${i}`,
   value1: Math.floor(Math.random() * 1000),
-  value2: Math.floor(Math.random() * 1000)
-}))
+  value2: Math.floor(Math.random() * 1000),
+}));
+
 
 export default function Home() {
-  title("Home")
-  const vehiclesByAreaCode = useApi("/vehicles/counts/by-area-code", "get")
+  title("Home");
+  const vehiclesByAreaCode = useApi("/vehicles/counts/by-area-code", "get");
 
   return (
     <>
-      <MainNavbar/>
+      <MainNavbar />
       <Container className={"mt-5 col-12 col-xxl-8 mx-auto mb-5"}>
         <MapAndTable apiData={vehiclesByAreaCode} />
         <Row className={"mt-5"}>
-          <VehiclesBarChart apiData={vehiclesByAreaCode}/>
+          <VehiclesBarChart apiData={vehiclesByAreaCode} />
           <div className={"col-12 col-xl-6"}>
-            <h3 className={style.chartTitle}>
-              Przykładowy wykres liniowy
-            </h3>
+            <h3 className={style.chartTitle}>Przykładowy wykres liniowy</h3>
             <ResponsiveContainer width={"100%"} height={300}>
               {/*problem same as above*/}
-              <AreaChart width={730} height={250} data={SAMPLE_DATA}
-                         margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+              <AreaChart
+                width={730}
+                height={250}
+                data={SAMPLE_DATA}
+                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+              >
                 <defs>
                   <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
                   </linearGradient>
                   <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                    <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-                    <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+                    <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                    <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
                   </linearGradient>
                 </defs>
-                <XAxis dataKey="name"/>
-                <YAxis/>
-                <CartesianGrid strokeDasharray="3 3"/>
-                <Tooltip/>
-                <Legend/>
-                <Area type="monotone" dataKey="value1" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)"/>
-                <Area type="monotone" dataKey="value2" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)"/>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip />
+                <Legend />
+                <Area
+                  type="monotone"
+                  dataKey="value1"
+                  stroke="#8884d8"
+                  fillOpacity={1}
+                  fill="url(#colorUv)"
+                />
+                <Area
+                  type="monotone"
+                  dataKey="value2"
+                  stroke="#82ca9d"
+                  fillOpacity={1}
+                  fill="url(#colorPv)"
+                />
               </AreaChart>
             </ResponsiveContainer>
           </div>
           <div className={"col-12 col-xl-6"}>
-            <h3 className={style.chartTitle}>
-              Przykładowy wykres kołowy
-            </h3>
+            <h3 className={style.chartTitle}>Przykładowy wykres kołowy</h3>
             <ResponsiveContainer width={"100%"} height={300}>
               <PieChart width={730} height={250}>
                 {/*this chart is a bit broken, but it's just an example*/}
-                <Pie data={SAMPLE_DATA} dataKey="value1" nameKey="name" cx="50%" cy="50%" outerRadius={80}
-                     fill="#8884d8"
-                     label/>
+                <Pie
+                  data={SAMPLE_DATA}
+                  dataKey="value1"
+                  nameKey="name"
+                  cx="50%"
+                  cy="50%"
+                  outerRadius={80}
+                  fill="#8884d8"
+                  label
+                />
               </PieChart>
             </ResponsiveContainer>
           </div>
           <div className={"col-12 col-xl-6"}>
-            <h3 className={style.chartTitle}>
-              Przykładowy wykres radarowy
-            </h3>
+            <h3 className={style.chartTitle}>Przykładowy wykres radarowy</h3>
             <ResponsiveContainer width={"100%"} height={300}>
               {/*this chart is not made for this number of data points, but still it's just an example*/}
-              <RadarChart outerRadius={90} width={730} height={250} data={SAMPLE_DATA}>
-                <PolarGrid/>
-                <PolarAngleAxis dataKey="name"/>
-                <Radar name="value1" dataKey="value1" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6}/>
-                <Radar name="value2" dataKey="value2" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6}/>
-                <PolarRadiusAxis angle={30} domain={[0, 150]}/>
-                <Legend/>
+              <RadarChart
+                outerRadius={90}
+                width={730}
+                height={250}
+                data={SAMPLE_DATA}
+              >
+                <PolarGrid />
+                <PolarAngleAxis dataKey="name" />
+                <Radar
+                  name="value1"
+                  dataKey="value1"
+                  stroke="#8884d8"
+                  fill="#8884d8"
+                  fillOpacity={0.6}
+                />
+                <Radar
+                  name="value2"
+                  dataKey="value2"
+                  stroke="#82ca9d"
+                  fill="#82ca9d"
+                  fillOpacity={0.6}
+                />
+                <PolarRadiusAxis angle={30} domain={[0, 150]} />
+                <Legend />
               </RadarChart>
             </ResponsiveContainer>
           </div>
         </Row>
       </Container>
     </>
-  )
+  );
 }
--- frontend/src/pages/home/components/MapAndTable.tsx
+++ frontend/src/pages/home/components/MapAndTable.tsx
@@ -1,48 +1,190 @@
 import style from "../Home.module.scss";
 import PolandMap from "./PolandMap.tsx";
-import {Row, Table} from "react-bootstrap";
-import {formatNumber} from "../../../util/utils.ts";
-import ChartStatusOverlay, {PropsWithApiData} from "./ChartStatusOverlay.tsx";
+import { Row, Table } from "react-bootstrap";
+import { formatNumber } from "../../../util/utils.ts";
+import ChartStatusOverlay, { PropsWithApiData } from "./ChartStatusOverlay.tsx";
+import { useState } from "react";
+import useApi from "../../../hooks/useApi.ts";
 
-export default function MapAndTable({apiData}: PropsWithApiData) {
+const INDICATORS = ["NO2", "NOx", "PM2.5", "Pb(PM10)", "SO2"];
+const YEARS = Array.from({ length: 24 }, (_, i) => 2000 + i);
+const VOIVODESHIPS = {
+  "02": "dolnośląskie",
+  "04": "kujawsko-pomorskie",
+  "06": "lubelskie",
+  "08": "lubuskie",
+  "10": "łódzkie",
+  "12": "małopolskie",
+  "14": "mazowieckie",
+  "16": "opolskie",
+  "18": "podkarpackie",
+  "20": "podlaskie",
+  "22": "pomorskie",
+  "24": "śląskie",
+  "26": "świętokrzyskie",
+  "28": "warmińsko-mazurskie",
+  "30": "wielkopolskie",
+  "32": "zachodniopomorskie",
+  XX: "nieokreślone",
+};
+
+// todo
+//przerobic, by mozna bylo wybrac tylko rok i w tabeli wszystkie dane sie wyswietla
+//przerobic pierwiastki by nie byly select tylko tablica z wybiraniem i mozliwosci usuwania
+//zrobic dobre wykresy
+
+export default function MapAndTable({ apiData }: PropsWithApiData) {
+  const [selectedIndicator, setSelectedIndicator] = useState(INDICATORS[0]);
+  const [selectedYear, setSelectedYear] = useState(YEARS[0]);
+  const [selectedVoivodeship, setSelectedVoivodeship] = useState("");
+
+  const airPollutionData = useApi(
+    `/air-pollution/counts/average-by-indicator-and-year?indicator=${selectedIndicator}&year=${selectedYear}`,
+    "get"
+  );
+
+  const handleIndicatorChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSelectedIndicator(event.target.value);
+  };
+
+  const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedYear(Number(event.target.value));
+  };
+
+  const findAirPollution = (voivodeship: string) => {
+    if (!airPollutionData.data) {
+      return "N/A";
+    }
+  
+    const data = airPollutionData.data.find(
+      ([name]: [string, number]) => name === voivodeship
+    );
+    return data ? data[1] : "N/A";
+  };
+
+  const pollutionData = apiData.data 
+  ? (Object.keys(apiData.data) as Array<keyof typeof VOIVODESHIPS>).reduce((acc, key) => {
+      const voivodeshipName = VOIVODESHIPS[key];
+      acc[voivodeshipName] = {
+        name: voivodeshipName,
+        averagePollution: findAirPollution(voivodeshipName)
+      };
+      return acc;
+    }, {} as Record<string, { name: string, averagePollution: number | 'N/A' }>)
+  : {};
+  
+  console.log(pollutionData);
+
   return (
-    <Row>
-      <div className={`text-center col-12 col-xxl-5 ${style.map} position-relative`}>
-        <ChartStatusOverlay apiData={apiData} backgroundType={"none"}/>
-        <PolandMap apiData={apiData}/>
-      </div>
-      <div className={`col-12 col-xxl-7 ${style.table}`}>
-        <Table bordered striped={apiData.data ? true : undefined}>
-          <thead>
-          <tr>
-            <th>Województwo</th>
-            <th>Liczba pojazdów</th>
-            <th>Jakies inne dane</th>
-          </tr>
-          </thead>
-          <tbody>
-          {apiData.data ? Object.keys(apiData.data).map((key) => (
-              <tr key={key}>
-                <td>{key}</td>
-                <td>{formatNumber(apiData.data[key])}</td>
-                <td>...</td>
+    <>
+      <Row>
+        <div className="container mb-3">
+          <div className="row align-items-center">
+            <div className="col-auto text-end pe-3">
+              <p className="mb-0">Pierwiastek:</p>
+            </div>
+            <div className="col-auto text-center">
+              <div className="">
+                <select
+                  value={selectedIndicator}
+                  onChange={handleIndicatorChange}
+                  className="form-select"
+                >
+                  {INDICATORS.map((indicator) => (
+                    <option key={indicator} value={indicator}>
+                      {indicator}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div className="col-auto text-end pe-3">
+              <p className="mb-0">Rok:</p>
+            </div>
+            <div className="col-auto text-center">
+              <div className="">
+                <select
+                  value={selectedYear}
+                  onChange={handleYearChange}
+                  className="form-select"
+                >
+                  {YEARS.map((year) => (
+                    <option key={year} value={year}>
+                      {year}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Row>
+      <Row>
+        <div
+          className={`text-center col-12 col-xxl-5 ${style.map} position-relative`}
+        >
+          <ChartStatusOverlay apiData={apiData} backgroundType={"none"} />
+          <PolandMap apiData={apiData} pollutionData={pollutionData}/>
+        </div>
+        <div className={`col-12 col-xxl-7 ${style.table}`}>
+          <Table bordered striped={apiData.data ? true : undefined}>
+            <thead>
+              <tr>
+                <th>Województwo</th>
+                <th>Liczba pojazdów</th>
+                <th>Średnie zanieczyszczenie {selectedIndicator}</th>
               </tr>
-            ))
-            : Array.from({length: 10}, (_, i) => <LoadingRow key={i} i={i}/>)
-          }
-          </tbody>
-        </Table>
-      </div>
-    </Row>
-  )
+            </thead>
+            <tbody>
+              {apiData.data
+                ? (
+                    Object.keys(apiData.data) as Array<
+                      keyof typeof VOIVODESHIPS
+                    >
+                  ).map((key) => {
+                    const voivodeshipName = VOIVODESHIPS[key];
+                    return (
+                      <tr key={key}>
+                        <td>{voivodeshipName}</td>
+                        <td>{formatNumber(apiData.data[key])}</td>
+                        <td>{findAirPollution(voivodeshipName)}</td>
+                      </tr>
+                    );
+                  })
+                : Array.from({ length: 10 }, (_, i) => (
+                    <LoadingRow key={i} i={i} />
+                  ))}
+            </tbody>
+          </Table>
+        </div>
+      </Row>
+    </>
+  );
 }
 
-function LoadingRow({i}: { i: number }) {
+function LoadingRow({ i }: { i: number }) {
   return (
     <tr>
-      <td className={style.loadingRow} style={{animationDelay: `${i * 0.1}s`}}>...</td>
-      <td className={style.loadingRow} style={{animationDelay: `${i * 0.1 + 0.1}s`}}>...</td>
-      <td className={style.loadingRow} style={{animationDelay: `${i * 0.1 + 0.2}s`}}>...</td>
+      <td
+        className={style.loadingRow}
+        style={{ animationDelay: `${i * 0.1}s` }}
+      >
+        ...
+      </td>
+      <td
+        className={style.loadingRow}
+        style={{ animationDelay: `${i * 0.1 + 0.1}s` }}
+      >
+        ...
+      </td>
+      <td
+        className={style.loadingRow}
+        style={{ animationDelay: `${i * 0.1 + 0.2}s` }}
+      >
+        ...
+      </td>
     </tr>
-  )
+  );
 }
