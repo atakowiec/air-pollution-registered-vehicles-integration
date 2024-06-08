21540d1ff0ba6e7471d323e5a7240bd4cdfbd289
atakowiec <mat.cieszczyk@gmail.com>
2024-06-09 01:14:18 +0200

    missing change

--- backend/src/main/java/pl/pollub/is/backend/pollution/AirPollutionController.java
+++ backend/src/main/java/pl/pollub/is/backend/pollution/AirPollutionController.java
@@ -1,14 +1,12 @@
 package pl.pollub.is.backend.pollution;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/pollution")
@@ -17,7 +15,7 @@ public class AirPollutionController {
     private final AirPollutionService airPollutionService;
 
     @PostMapping("/import")
-    public Map<String, Integer> handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
         return airPollutionService.handleFileUpload(file);
     }
 }
