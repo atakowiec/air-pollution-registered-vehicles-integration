621f5d079cca6bc92b7f8c52ae0ac058808cf3fb
atakowiec <mat.cieszczyk@gmail.com>
2024-06-12 19:23:38 +0200

    query fixes

--- backend/src/main/java/pl/pollub/is/backend/vehicles/VehiclesRepository.java
+++ backend/src/main/java/pl/pollub/is/backend/vehicles/VehiclesRepository.java
@@ -9,10 +9,10 @@ import java.util.List;
 
 public interface VehiclesRepository extends JpaRepository<Vehicle, Integer> {
 
-    @Query(value = "SELECT v.areaCode, COUNT(v) FROM Vehicle v WHERE v.fuelType is null GROUP BY v.areaCode")
+    @Query(value = "SELECT v.areaCode, COUNT(v) FROM Vehicle v WHERE v.fuelType is not null GROUP BY v.areaCode")
     List<Object[]> countVehiclesByAreaCode();
 
-    @Query(value = "SELECT * FROM vehicles WHERE fuel_type is null " +
+    @Query(value = "SELECT * FROM vehicles WHERE fuel_type is not null " +
             "AND area_code = :areaCode " +
             "AND first_registration_date <= CONCAT(:year, '-12-31') " +
             "AND (deregistration_date IS NULL OR deregistration_date >= CONCAT(:year, '-01-01'))",
@@ -24,7 +24,7 @@ public interface VehiclesRepository extends JpaRepository<Vehicle, Integer> {
 
     @Query(value = "SELECT area_code, COUNT(*) FROM vehicles " +
             "WHERE first_registration_date <= CONCAT(:year, '-12-31') " +
-            "AND v.fuelType is null " +
+            "AND v.fuelType is not null " +
             "AND (deregistration_date IS NULL OR deregistration_date >= CONCAT(:year, '-01-01')) " +
             "GROUP BY area_code",
             nativeQuery = true)
@@ -33,7 +33,7 @@ public interface VehiclesRepository extends JpaRepository<Vehicle, Integer> {
     @Query(value = "SELECT year(first_registration_date) as year, area_code, count(*) FROM `vehicles`" +
             "WHERE first_registration_date IS NOT NULL " +
             "AND year(first_registration_date) BETWEEN 1900 AND 2019 " +
-            "AND fuel_type is null " +
+            "AND fuel_type is not null " +
             "GROUP BY year(first_registration_date), area_code",
             nativeQuery = true)
     List<Object[]> countRegistrationsByYearAndVoivodeship();
@@ -41,12 +41,12 @@ public interface VehiclesRepository extends JpaRepository<Vehicle, Integer> {
     @Query(value = "SELECT year(deregistration_date) as year, area_code, count(*) FROM `vehicles` " +
             "WHERE deregistration_date is not null " +
             "AND year(deregistration_date) BETWEEN 1900 AND 2019 " +
-            "AND fuel_type is null " +
+            "AND fuel_type is not null " +
             "GROUP BY year(deregistration_date), area_code;",
             nativeQuery = true)
     List<Object[]> countDeregistrationsByYearAndVoivodeship();
 
-    @Query(value = "SELECT v.areaCode, COUNT(*) FROM Vehicle v WHERE v.deregistrationDate IS NOT NULL AND v.fuelType is null GROUP BY v.areaCode")
+    @Query(value = "SELECT v.areaCode, COUNT(*) FROM Vehicle v WHERE v.deregistrationDate IS NOT NULL AND v.fuelType is not null GROUP BY v.areaCode")
     List<Object[]> countDeregistrationsByAreaCode();
 
     @Query(value = "SELECT v.fuelType, COUNT(v) FROM Vehicle v WHERE v.fuelType IS NOT NULL AND v.fuelType != 'NIE DOTYCZY' GROUP BY v.fuelType")
